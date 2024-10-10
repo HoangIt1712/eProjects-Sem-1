468d9a3e4e00564999b26581ccd63fd4644090f7
nguyenquockhanh2301 <149218424+nguyenquockhanh2301@users.noreply.github.com>
2024-10-10 20:00:44 +0700

    Kruwul
    Fix web Blog
    Add file head and footer in folder "html"

--- blog2.php
+++ blog2.php
@@ -6,169 +6,26 @@
 	<meta content="" name="keywords">
     <meta content="" name="description">
     <title>Travel - Bronx Luggage</title>
-    <link rel="stylesheet" type="text/css" href="/bootstrap4/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style1.css">
-    <link rel="stylesheet" href="css/main_styles.css">
-
-	<link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="css/style1.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+    <link rel="stylesheet" type="text/css" href="styles/main_styles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" type="text/css" href="styles/responsive.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/product.css?v=<?php echo time(); ?>">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="css/home.css" rel="stylesheet">
 </head>
 <body>
 
 <div class="super_container">
-
-<!-- Header -->
-
-<header class="header trans_300">
-
-    <!-- Top Navigation -->
-
-    <div class="top_nav">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="top_nav_left">free shipping on all u.s orders over $50</div>
-                </div>
-                <div class="col-md-6 text-right">
-                    <div class="top_nav_right">
-                        <ul class="top_nav_menu">
-
-                            <!-- Currency / Language / My Account -->
-
-                            <li class="currency">
-                                <a href="#">
-                                    usd
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                                <ul class="currency_selection">
-                                    <li><a href="#">cad</a></li>
-                                    <li><a href="#">aud</a></li>
-                                    <li><a href="#">eur</a></li>
-                                    <li><a href="#">gbp</a></li>
-                                </ul>
-                            </li>
-                            <li class="language">
-                                <a href="#">
-                                    English
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                                <ul class="language_selection">
-                                    <li><a href="#">French</a></li>
-                                    <li><a href="#">Italian</a></li>
-                                    <li><a href="#">German</a></li>
-                                    <li><a href="#">Spanish</a></li>
-                                </ul>
-                            </li>
-                            <li class="account">
-                                <a href="#">
-                                    My Account
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                                <ul class="account_selection">
-                                    <li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a>
-                                    </li>
-                                    <li><a href="RegisterPage.php"><i class="fa fa-user-plus"
-                                                aria-hidden="true"></i>Register</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Main Navigation -->
-
-<div class="main_nav_container">
-<div class="container">
-<div class="row">
-    <div class="col-lg-12 text-right">
-        <div class="logo_container">
-        <a href="index.php">Bronx<span>Luggage</span></a>
-        </div>
-        <nav class="navbar">
-            <ul class="navbar_menu">
-                <li><a href="index.php">home</a></li>
-                <li><a href="category.php">shop</a></li>
-                <li><a href="page.php">pages</a></li>
-                <li><a href="contact.html">contact</a></li>
-            </ul>
-            <ul class="navbar_user">
-                <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-                <li class="checkout">
-                    <a href="#">
-                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                    </a>
-                </li>
-            </ul>
-            
-        </nav>
-    </div>
-</div>
-</div>
-</div>
-</header>
-
+<?php include("html/head.php"); ?>
+<div class="blog2">
 <div class="fs_menu_overlay"></div>
-<div class="hamburger_menu">
-    <div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
-    <div class="hamburger_menu_content text-right">
-        <ul class="menu_top_nav">
-            <li class="menu_item has-children">
-                <a href="#">
-                    usd
-                    <i class="fa fa-angle-down"></i>
-                </a>
-                <ul class="menu_selection">
-                    <li><a href="#">cad</a></li>
-                    <li><a href="#">aud</a></li>
-                    <li><a href="#">eur</a></li>
-                    <li><a href="#">gbp</a></li>
-                </ul>
-            </li>
-            <li class="menu_item has-children">
-                <a href="#">
-                    English
-                    <i class="fa fa-angle-down"></i>
-                </a>
-                <ul class="menu_selection">
-                    <li><a href="#">French</a></li>
-                    <li><a href="#">Italian</a></li>
-                    <li><a href="#">German</a></li>
-                    <li><a href="#">Spanish</a></li>
-                </ul>
-            </li>
-            <li class="menu_item has-children">
-                <a href="#">
-                    My Account
-                    <i class="fa fa-angle-down"></i>
-                </a>
-                <ul class="menu_selection">
-                    <li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-                    <li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
-                </ul>
-            </li>
-            <li class="menu_item"><a href="index.php">home</a></li>
-            <li class="menu_item"><a href="category.php">shop</a></li>
-            <li class="menu_item"><a href="page.php">pages</a></li>
-            <li class="menu_item"><a href="blog1.php">blog</a></li>
-            <li class="menu_item"><a href="contact.html">contact</a></li>
-        </ul>
-    </div>
-</div>
 
-</header></br></br>
     <div class="container1">
         <h1>Essential travel accessories for every trip</h1>
         <p>
@@ -227,50 +84,17 @@
         <h4> These accessories not only help you maximize suitcase space but also ensure a safer, more comfortable, and organized trip. </h4>
         <h4><a href="page.php" class="btn">Back</a></h4>
     </div>
-    <!-- Footer Start -->
-<div class="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Address</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Services</h4>
-                    <a class="btn btn-link" href="">Maintenance</a>
-                    <a class="btn btn-link" href="">Transport</a>
-                    <a class="btn btn-link" href="">Promotion</a>
-                    <a class="btn btn-link" href="">Dear Customer</a>
-                    <a class="btn btn-link" href="">On request</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="">About Us</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Our Services</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">Support</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
     </div>
+    <!-- Footer Start -->
+    <?php include("html/footer.php"); ?>
+
     <!-- Footer End -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="styles/bootstrap4/popper.js"></script>
+        <script src="styles/bootstrap4/bootstrap.min.js"></script>
+        <script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+        <script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+        <script src="plugins/easing/easing.js"></script>
+        <script src="js/custom.js"></script>
 </body>
 </html>
